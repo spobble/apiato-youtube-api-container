e8ceb0c439e33554cd0af43a578d518415ad33c9
Andrea Civita <andreacivita994@gmail.com>
2020-10-13 15:36:11 +0200

    Update Controller.php

--- UI/WEB/Controllers/Controller.php
+++ UI/WEB/Controllers/Controller.php
@@ -6,6 +6,8 @@ use Apiato\Core\Foundation\Facades\Apiato;
 use App\Ship\Parents\Controllers\WebController;
 use App\Ship\Transporters\DataTransporter;
 
+use App\Containers\Club\UI\WEB\Requests\GetRefreshTokenRequest;
+
 class Controller extends WebController
 {
     public function index()
@@ -13,5 +15,12 @@ class Controller extends WebController
         $link = Apiato::call('YoutubeApi@GenerateOAuthLoginAction');
 
         return redirect($link);
+    }
+
+    public function callback(GetRefreshTokenRequest $request)
+    {
+        $result = Apiato::call('YoutubeApi@UploadVideoAction',[$request]);
+
+        print_r($result); die;
     }
 }
